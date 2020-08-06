745490776a00e5803ec86ea5084a4e98d432569e
Jeffrey S Whitaker <jeffrey.s.whitaker@noaa.gov>
2020-08-06 13:22:59 -0500

    updates for non-iau

--- src/gsi/gsimod.F90
+++ src/gsi/gsimod.F90
@@ -59,7 +59,7 @@
                        l4dvar,nhr_obsbin,nhr_subwin,nwrvecs,iorthomax,&
                        lbicg,lsqrtb,lcongrad,lbfgsmin,ltlint,ladtest,ladtest_obs, lgrtest,&
                        idmodel,clean_4dvar,iwrtinc,lanczosave,jsiga,ltcost,liauon, &
-		       l4densvar,lhourly_da,liau,lfg_only,ens_nstarthr,lnested_loops,lwrite4danl,nhr_anal,thin4d,tau_fcst,efsoi_order
+		       l4densvar,lhourly_da,liau,lfirst_hrly_analysis,ens_nstarthr,lnested_loops,lwrite4danl,nhr_anal,thin4d,tau_fcst,efsoi_order
   use gsi_4dvar, only: mPEs_observer
   use m_obsdiags, only: alwaysLocal => obsdiags_alwaysLocal
   use obs_ferrscale, only: lferrscale
@@ -592,7 +592,7 @@
 !     l4densvar - logical to turn on ensemble 4dvar
 !     lhourly_da- logical to turn on hourly da with overlapping windows
 !     liau      - logical to turn on hourly da with overlapping windows - the IAU part
-!     lfg_only  - logical for no-IAU in overlapping windows to use f02 or f01
+!     lfirst_hrly_analysis  - logical for no-IAU in overlapping windows to use f02 or f01
 !     ens_nstarthr - start hour for ensemble perturbations (generally should match min_offset)
 !     lwrite4danl - logical to write out 4d analysis states if 4dvar or 4denvar mode
 !     nhr_anal - forecast hours to write out if lwrite4danal=T
@@ -678,8 +678,8 @@
        lferrscale,print_diag_pcg,tsensible,lgschmidt,lread_obs_save,lread_obs_skip, &
        use_gfs_ozone,check_gfs_ozone_date,regional_ozone,lwrite_predterms,&
        lwrite_peakwt,use_gfs_nemsio,use_gfs_ncio,sfcnst_comb,liauon,use_prepb_satwnd,l4densvar,lhourly_da,liau,&
-       lfg_only,ens_nstarthr,use_gfs_stratosphere,pblend0,pblend1,step_start,diag_precon,lrun_subdirs,&
-       use_sp_eqspace,lnested_loops,lsingleradob,thin4d,use_readin_anl_sfcmask,&
+       lfirst_hrly_analysis,ens_nstarthr,use_gfs_stratosphere,pblend0,pblend1,step_start,diag_precon,&
+       lrun_subdirs,use_sp_eqspace,lnested_loops,lsingleradob,thin4d,use_readin_anl_sfcmask,&
        luse_obsdiag,id_drifter,id_ship,verbose,print_obs_para,lsingleradar,singleradar,lnobalance, &
        missing_to_nopcp,minobrangedbz,minobrangedbz,maxobrangedbz,&
        maxobrangevr,maxtiltvr,whichradar,doradaroneob,oneoblat,&
