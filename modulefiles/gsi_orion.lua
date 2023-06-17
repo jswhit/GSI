762de074136d580a379d7227ce075b3ce3bbafcb
Jeffrey S Whitaker <jeffrey.s.whitaker@noaa.gov>
2023-06-17 14:38:07 -0500

    fixes for Orion

--- modulefiles/gsi_orion.lua
+++ modulefiles/gsi_orion.lua
@@ -3,9 +3,9 @@ help([[
 
 prepend_path("MODULEPATH", "/apps/contrib/NCEP/libs/hpc-stack/modulefiles/stack")
 
-local hpc_ver=os.getenv("hpc_ver") or "1.1.0"
-local hpc_intel_ver=os.getenv("hpc_intel_ver") or "2018.4"
-local hpc_impi_ver=os.getenv("hpc_impi_ver") or "2018.4"
+local hpc_ver=os.getenv("hpc_ver") or "1.2.0"
+local hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.1.2"
+local hpc_impi_ver=os.getenv("hpc_impi_ver") or "2022.1.2"
 local cmake_ver=os.getenv("cmake_ver") or "3.22.1"
 local python_ver=os.getenv("python_ver") or "3.7.5"
 local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
@@ -23,6 +23,6 @@ load(pathJoin("prod_util", prod_util_ver))
 pushenv("CFLAGS", "-xHOST")
 pushenv("FFLAGS", "-xHOST")
 
-pushenv("GSI_BINARY_SOURCE_DIR", "/work/noaa/global/glopara/fix/gsi/20221128")
+pushenv("GSI_BINARY_SOURCE_DIR", "/work/noaa/global/glopara/fix/gsi/20230601")
 
 whatis("Description: GSI environment on Orion with Intel Compilers")
